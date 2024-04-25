b3d29cf351e8210d2f617ded33c9b9fb2dd1cd9d
Mustafas72 <Sanavadwalamustafa@gmail.com>
2024-04-25 14:04:49 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -11,6 +11,7 @@ function smallestMissingPositiveInteger(nums) {
   // Iterate from 1 until we find the smallest missing positive integer
   for (let i = 1; i <= Math.max(...nums) + 1; i++) {
       if (!numSet.has(i)) {
+        console.log("for", nums, "the answer is", i);
           return i;
       }
   }
