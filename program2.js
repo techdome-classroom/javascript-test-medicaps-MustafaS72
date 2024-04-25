b89011add0eb946c076bb5e617e8142710b5dfef
Mustafas72 <Sanavadwalamustafa@gmail.com>
2024-04-25 13:47:19 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,19 @@
 function smallestMissingPositiveInteger(nums) {
+  // Remove negative numbers and zeros
+  nums = nums.filter(num => num > 0);
 
-  // Implement the function smallest_missing_positive_integer
+  // Initialize a variable to track the smallest positive integer
+  let smallestPositive = 1;
 
+  // Iterate through the positive numbers
+  for (const num of nums) {
+      // If the current number is equal to the smallest positive integer, increment smallestPositive
+      if (num === smallestPositive) {
+          smallestPositive++;
+      }
+  }
+
+  return smallestPositive;
 }
 
 module.exports = smallestMissingPositiveInteger;
