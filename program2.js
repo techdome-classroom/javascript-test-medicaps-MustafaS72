260929e9f52cc83cd34c92801db6f19ccd7259b6
Mustafas72 <Sanavadwalamustafa@gmail.com>
2024-04-25 13:46:08 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,16 +1,14 @@
 function smallestMissingPositiveInteger(nums) {
-  // Filter out negative numbers and zeros
-  const positiveNums = nums.filter(num => num > 0);
+  const posNos = nums.filter(num => num > 0);//Filtering the -ve Nos ans 0
+    const numSet = new Set(posNos); //Creating set for +ve integers
 
-  // Create a set to store positive integers
-  const numSet = new Set(positiveNums);
+    //check from index 1 and check whether it exists in the set or not
+    for (var i = 1; i <= posNos.length + 1; i++) {
+        if (!numSet.has(i)) {
+            return i;
+        }
+    }
 
-  // Start from 1 and check if it exists in the set
-  for (let i = 1; i <= positiveNums.length + 1; i++) {
-      if (!numSet.has(i)) {
-          return i;
-      }
-  }
 }
 
 module.exports = smallestMissingPositiveInteger;
