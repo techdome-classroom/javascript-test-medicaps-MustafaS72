38f9261a207f8bcad31aa6cc3bb396d957099179
Mustafas72 <Sanavadwalamustafa@gmail.com>
2024-04-25 13:35:46 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,19 @@
 function longestSubstring(s) {
-    
+    let maxLength = 0;
+    let start = 0;
+    let charIndexMap = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const currentChar = s[i];
+        if (charIndexMap[currentChar] >= start) {
+            start = charIndexMap[currentChar] + 1;
+        }
+        charIndexMap[currentChar] = i;
+        const currentLength = i - start + 1;
+        maxLength = Math.max(maxLength, currentLength);
+    }
+
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
